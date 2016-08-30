e5b8bd80e99b5f9fc1b535e4888715bf4508bfe3
footclan2k2 <ryan@hugsbyha.com>
2016-08-30 14:35:07 -0400

    added a mixin

--- js/factories/team.factory.js
+++ js/factories/team.factory.js
@@ -7,7 +7,7 @@
 		
 
 	var teamMembers = [
-			{"image": "images/team/members/miles.png", "name": "Miles", "title": "Executive Director"},
+			{"image": "images/team/members/miles.png", "name": "Miles", "title": "Executive"},
 			{"image": "images/team/members/ryan.png", "name": "Ryan", "title": "Media Director"},
 			{"image": "images/team/members/lauren.png", "name": "Lauren", "title": "CEO Director"},
 			{"image": "images/team/members/willem.png", "name": "Willem", "title": "Vice Director"}
