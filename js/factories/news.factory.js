474188ce7b29e61cc990ab721794685a92a411f2
footclan2k2 <ryan@hugsbyha.com>
2016-09-06 17:23:24 -0400

    styling for mobile news is done

--- js/factories/news.factory.js
+++ js/factories/news.factory.js
@@ -5,11 +5,11 @@
 		.factory("NewsService", NewsService);
 
 	var newsStories = [
-			{"image": "images/news/vice.png", "date": "some date", "header": "some header", "story": "some info", "url": "www.google.com"},
-			{"image": "images/news/vice.png", "date": "some date2", "header": "some header2", "story": "some info2", "url": "www.google.com"},
-			{"image": "images/news/vice.png", "date": "some date3", "header": "some header3", "story": "some info3", "url": "www.google.com"},
-			{"image": "images/news/vice.png", "date": "some date4", "header": "some header4", "story": "some info4", "url": "www.google.com"},
-			{"image": "images/news/vice.png", "date": "some date5", "header": "some header5", "story": "some info5", "url": "www.google.com"}
+			{"image": "images/news/vice.png", "author": "company", "date": "some date", "header": "some header", "story": "some info", "url": "www.google.com"},
+			{"image": "images/news/vice.png", "author": "company", "date": "some date2", "header": "some header2", "story": "some info2", "url": "www.google.com"},
+			{"image": "images/news/vice.png", "author": "company", "date": "some date3", "header": "some header3", "story": "some info3", "url": "www.google.com"},
+			{"image": "images/news/vice.png", "author": "company", "date": "some date4", "header": "some header4", "story": "some info4", "url": "www.google.com"},
+			{"image": "images/news/vice.png", "author": "company", "date": "some date5", "header": "some header5", "story": "some info5", "url": "www.google.com"}
 		];
 
 		function NewsService (){
